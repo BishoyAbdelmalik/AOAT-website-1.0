acba0d44ba543d42360ff63dfe8bb91b25e61b74
Bishoy Abdelmalik <mrboushy@users.noreply.github.com>
2017-10-05 12:29:39 -0700

    new ideas

--- newideas/academics.php
+++ newideas/academics.php
@@ -120,6 +120,20 @@
                         <h3>AOAT Curriculum</h3>
                         <section id="curriculum">
                             <section>
+                                <h4>Grade 9: Pre Academy</h4>
+                                <h5>Team Leader: <a href="teachers.php?t=rosillo">Gabriela Rosillo</a></h5>
+                                <p>
+
+                                </p>
+                                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#10th">View Classes</button>
+                                <div id="10th" class="collapse">
+                                    <h5><b>Classes</b></h5>
+                                    <ul>
+                                        <li></li>
+                                    </ul>
+                                </div>
+                            </section>
+                            <section>
                                 <h4>Grade 10: Theme: Awareness</h4>
                                 <h5>Team Leader: <a href="teachers.php?t=prato">Paola Prato</a></h5>
                                 <p>
