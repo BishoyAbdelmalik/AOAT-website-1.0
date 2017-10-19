52fd734773d2315e92a7b1914c49a0624d97f92d
Bishoy Abdelmalik <mrboushy@users.noreply.github.com>
2017-10-18 19:41:42 -0700

    student_work

--- newideas/student_work.php
+++ newideas/student_work.php
@@ -7,91 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="css/student_work.css" />
     <title>AOAT | Student Work</title>
-    <style type="text/css">
-        #intro {
-            padding-top: 27px;
-            padding-bottom: 27px;
-        }
 
-        .container {
-            width: 75%;
-            margin: auto;
-        }
-
-        .banner {
-            margin-bottom: 0px;
-        }
-
-        main section {
-            cursor: pointer;
-        }
-
-        .studentwork {
-            color: #fff;
-            margin: 0;
-            width: 100%;
-            float: left;
-            /* background: #c4003e;*/
-            background: #3094d6;
-
-        }
-
-        .studentwork h3 {
-            margin: 0;
-        }
-
-        .studentwork p {
-            margin: 0;
-        }
-
-        .studentwork article {
-            padding: 2em;
-            width: 33.33%;
-            text-align: center;
-            float: left;
-            box-sizing: border-box;
-        }
-
-        .studentwork article p {
-            font-size: 1em;
-        }
-
-        .studentwork article h3 {
-            font-size: 2em;
-            margin-bottom: 10px;
-
-        }
-
-        .studentwork img {
-            float: left;
-            width: 33.33%;
-        }
-
-        .studentwork a img {
-            background: #fff;
-        }
-
-        .studentworkTile {
-            background: #fff;
-            width: 33.33%;
-            float: left;
-        }
-
-        .studentworkTile img {
-
-            transition: all 500ms;
-            opacity: .4;
-            width: 50%;
-        }
-
-        .studentworkTile img:hover {
-
-
-            opacity: 0.8;
-        }
-
-    </style>
 
 </head>
 
@@ -103,81 +21,93 @@
         ?>
     </header>
     <div class="banner" id="student_work">
-
-        <h2 class="pages_title">Student Work</h2>
+        <div class="container-fluid lockcontent">
+            <h2 class="pages_title">Student Work</h2>
+        </div>
     </div>
     <sevtion>
-        <div id="intro" class="lockcontent">
+        <div id="intro" class="container-fluid lockcontent">
             AOAT trains students in graphic design and photography for print and web, and develops their skills in design software such as Adobe Creative Suite. Many of their projects are connected to their AOAT academic classes such as science, English, and history/government.
         </div>
     </sevtion>
-    <main>
+    <main class="blue_background">
+        <section class="studentwork flex_b wrap">
 
-        <section class="studentwork">
-            <a>
-                <article>
+            <article class="flex_b ">
+                <div>
                     <h3>Digital Photography</h3>
 
                     <p>
                         AOAT trains students in graphic design and photography for print and web, and develops their skills in design software such as Adobe Creative Suite. Many of their projects are connected to their AOAT academic classes such as science, English, and history/government.
                     </p>
-
-                </article>
-                <img src="images/studentworkdpCover.jpg">
-            </a>
-            <div class="studentworkTile">
+                </div>
+            </article>
+            <img class="center_img" src="images/studentworkdpCover.jpg">
+            <div class="studentworkTile flex_b wrap">
                 <img src="images/studentworkdpTile1.jpg">
                 <img src="images/studentworkdpTile2.jpg">
                 <img src="images/studentworkdpTile3.jpg">
                 <img src="images/studentworkdpTile4.jpg">
             </div>
         </section>
-        <section class="studentwork">
-            <div class="studentworkTile">
+        <section class="studentwork flex_b wrap">
+            <div class="studentworkTile flex_b wrap">
                 <img src="images/studentworkdillTile1.jpg">
                 <img src="images/studentworkdillTile2.jpg">
                 <img src="images/studentworkdillTile3.jpg">
                 <img src="images/studentworkdillTile4.jpg">
             </div>
             <img src="images/studentworkdillCover.jpg">
-            <article>
-                <h3>Digital Illustration</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <article class="flex_b ">
+                <div>
+                    <h3>Digital Illustration</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
             </article>
         </section>
-        <section class="studentwork">
-            <article>
-                <h3>Digital Imaging</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <section class="studentwork flex_b wrap">
+
+            <article class="flex_b">
+                <div>
+                    <h3>Digital Imaging</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
             </article>
             <img src="images/album_cover.jpg">
-            <div class="studentworkTile">
+            <div class="studentworkTile flex_b wrap">
                 <img src="images/studentworkTile1.jpg">
                 <img src="images/studentworkTile2.jpg">
                 <img src="images/studentworkTile3.jpg">
                 <img src="images/studentworkTile4.jpg">
             </div>
         </section>
-        <section class="studentwork">
-            <div class="studentworkTile">
+        <section class="studentwork flex_b wrap">
+            <div class="studentworkTile flex_b wrap">
                 <img src="images/studentworkgdTile1.jpg">
                 <img src="images/studentworkgdTile2.jpg">
                 <img src="images/studentworkgdTile3.jpg">
                 <img src="images/studentworkgdTile4.jpg">
             </div>
+
             <img src="images/studentworkgdCover.jpg">
-            <article>
-                <h3>Graphic Design</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <article class="flex_b">
+                <div>
+                    <h3>Graphic Design</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
             </article>
         </section>
-        <section class="studentwork">
-            <article>
-                <h3>Web Design</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <section class="studentwork flex_b wrap">
+
+
+            <article class="flex_b">
+                <div>
+                    <h3>Web Design</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
             </article>
             <img src="images/studentworkwdCover.jpg">
-            <div class="studentworkTile">
+            <div class="studentworkTile flex_b wrap">
                 <img src="images/studentworkwdTile1.jpg">
                 <img src="images/studentworkwdTile2.jpg">
                 <img src="images/studentworkwdTile3.jpg">
