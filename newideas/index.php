65de94adce6d5d4fea3d6025b6be4c174062e0aa
Bishoy Abdelmalik <mrboushy@users.noreply.github.com>
2017-10-28 17:56:18 -0700

    home screen adjustments

--- newideas/index.php
+++ newideas/index.php
@@ -31,17 +31,7 @@
             </div>
         </div>
     </div>
-    <!-- <div class="homeimage aboutAOAT landing">
-        <img class="landing_img" src="images/home/AboutAOAT.jpg">
-        <div class="absolute-text" id="aboutAOAT_AT">
-            <h1>About AOAT</h1>
-            <p> Our goal is to provide students with the academic background and professional tools necessary for success in any endeavor.</p>
-            <div class="arrow bounce">
-                <button class="fa fa-arrow-down fa-2x " onClick="runclick();" href="#"></button>
 
-            </div>
-        </div>
-    </div>-->
     <main style="background: #009cd5; ">
 
 
