fa347621ad893c05ef9ab5d70e83265b3aa2d8ce
Bishoy Abdelmalik <mrboushy@users.noreply.github.com>
2017-12-04 12:07:36 -0800

    clubs

--- newideas/aoat_clubs.php
+++ newideas/aoat_clubs.php
@@ -31,7 +31,7 @@
     <section id="first_section">
         <div id="intro" class="container-fluid lockcontent">
             <p class="justify gray_txt">
-                AOAT offers many different activities and clubs for the students to get involved and get to help the comunity in variety of ways. Some of these clubs are art related and focus more on artistic activities and they try to help the comunity while doing it in a creative and unique ways. Other clubs are mainly focused on the academic aspect of the school and helping students acheive their potential in school .
+                AOAT offers many different clubs for the students to get involved and get to help the comunity in variety of ways. Some of these clubs focus more on artistic activities and they try to help the comunity in a creative and unique ways. Other clubs are mainly focused on the academic aspect of the school and helping students acheive their potential in school.
             </p>
         </div>
     </section>
