95eb58baa04d2e14cd18ecf5a73af5f1b31994cc
Bishoy Abdelmalik <mrboushy@users.noreply.github.com>
2017-12-22 18:25:34 -0800

    admin

--- newideas/admin/db.php
+++ newideas/admin/db.php
@@ -1,4 +1,18 @@
 <?php
-require 'mysql.php';
-
+function update($db, $new_info, $id){
+    require 'mysql.php';
+    $query='DELETE FROM `'.$db.'` WHERE id='.$id.';';
+    mysqli_query($connection,$query);
+    $query='INSERT INTO `'.$db.'` (`id`, ';
+    foreach ($new_info as $key => $value){
+        $query.='`'.$key.'`, ';
+    }
+    $query.=") "
+    $query.='VALUES ('19', ';
+    foreach ($new_info as $key => $value){
+        $query.="'".$value."', ";        
+    }
+    $query.=');';
+    mysqli_query($connection,$query);    
+}
 ?>
