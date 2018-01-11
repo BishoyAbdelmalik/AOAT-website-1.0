233a9762fc0a1c6b1b387216edd1e9e51624cc6b
Bishoy Abdelmalik <mrboushy@users.noreply.github.com>
2018-01-11 12:21:51 -0800

    new design

--- v0.3/academics.php
+++ v0.3/academics.php
@@ -38,10 +38,9 @@
         <section class="flex_b container-fluid lockcontent">
             <div class="sidebuttons flex_b flex_column">
                 <button class="sidebtn" value="#Career">Career Development</button>
-                <button class="sidebtn" value="#mission">Our Mission</button>
-                <button class="sidebtn" value="#CPA">CPA Lighthouse Academy</button>
-                <button class="sidebtn" value="#Advisory">Board of Advisory</button>
-                <button class="sidebtn" value="#Partners">Partners</button>
+                <button class="sidebtn" value="#internship">Internships</button>
+                <button class="sidebtn" value="#Peer">Peer Mentors</button>
+                <button class="sidebtn" value="#Curriculum">AOAT Curriculum</button>
             </div>
             <div id="info">
                 <article id="Career">
@@ -67,8 +66,169 @@
                         <img class="img" src="images/academics/mock/mock2.jpg" />
                     </section>
                 </article>
+                <article id="internship">
+                    <h3>Internships</h3>
+                    <section class="gray_txt">
+
+                        <section class="pointer">
+
+                            <p class="justify  internship">
+                                Since 2011, summer internships have provided life-changing experiences for students, preparing them with real world skills and confidence to take on life as adults after graduation. Many of our summer hosts repeat from year to year, which makes the process easier and proves that the benefits go both ways. Our Internship Teacher Team puts in long hours to make sure there are enough appropriate host sites to accommodate and mentor all our candidates for the six weeks of exploratory work experience.
+                            </p>
+                        </section>
+                        <section id="art_club" class="flex_b ">
+                            <div class="flex_b">
+                                <div>
+                                    <h4>The process of preparing 11th graders for their summer internship includes:</h4>
+                                    <ul>
+                                        <li>Workshops and a class Career Exploration visit</li>
+                                        <li>Designing a personal logo and business stationery</li>
+                                        <li>Creating a formal resumé</li>
+                                        <li>Job shadow in career field of their interest</li>
+                                        <li>Career interest assessment and research</li>
+                                        <li>Parent informational meeting</li>
+                                        <li>Workshops connected to Career Day</li>
+                                        <li>Mock interviews</li>
+                                        <li>Application requiring GPA confirmation and parent approval</li>
+                                        <li>Matching of intern candidates and host sites</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <img class="sec_img" src="images/academics/intern/intern3.jpg" style="margin-left: 3%;">
+                        </section>
+                        <section class="container-fluid lockcontent pointer">
+                            <p class="justify  internship">
+                                During the 6-week internship, students attend a 2-hour seminar every week and work at their host site a minimum of 20 hours per week. AOAT teachers visit the interns at their jobs twice during the six weeks to insure that both student and host are benefiting from the experience. Interns prepare a culminating project beyond their regular work assignments and present it at a final celebration which brings together students and their families and the host businesses to recognize the successful completion of the summer internship.
+                            </p>
+                        </section>
+                    </section>
+
+                </article>
+                <article id="Peer">
+                    <section class="flex_b ">
+                        <section class="flex_b">
+                            <div>
+                                <h3>Peer Mentors</h3>
+                                <p class="justify">
+                                    Each year the 11th and 12th grade students are invited to participate in our Peer Mentor program, and are matched with a 9th or 10th grader who has self-selected to receive this support. The mentors and mentees meet during lunch once a week, and can either help with homework, tests, or personal issues that the younger students would like to talk about.
+                                </p>
+                            </div>
+                        </section>
+                        <img class=" img" src="images/clubs/mentor.jpg" style="margin-left: 14px;margin-bottom: 9px;">
+                    </section>
+                </article>
+                <article id="Curriculum">
+                    <h3>AOAT Curriculum</h3>
+                    <section id="curriculum_main">
+                        <section id="curriculum">
+                            <section>
+                                <h4>Grade 9: Pre-Academy</h4>
+                                <h5>Team Leader: <a href="teachers.php?t=rosillo">Gabriela Rosillo</a></h5>
+                                <p>
+
+                                </p>
+                                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#9th">View Classes</button>
+                                <div id="9th" class="collapse">
+                                    <h5><b>Classes</b></h5>
+                                    <ul>
+                                        <li>Intro Art.</li>
+                                        <li>English 9</li>
+                                        <li>Environmental Science</li>
+                                    </ul>
+                                </div>
+                            </section>
+                            <section>
+                                <h4>Grade 10: Theme: Awareness</h4>
+                                <h5>Team Leader: <a href="teachers.php?t=prato">Paola Prato</a></h5>
+                                <p class="justify">
+                                    The overall theme for 10th Grade is Social Group Awareness, and students explore what community means, how group identity is vital to change and how to create community. The theme of social justice, as reflected in literature and history, is the lens through which students examine the elements that contribute to how each individual can influence his community and the world. The interdisciplinary trip is a tour of the Museum of Tolerance.
+                                </p>
+                                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#10th">View Classes</button>
+                                <div id="10th" class="collapse">
+                                    <h5><b>Classes</b></h5>
+                                    <ul>
+                                        <li>
+                                            Photo
+                                            <ul>
+                                                <li>Digital Imaging</li>
+                                                <li>Digital Illustration</li>
+                                                <li>Photography</li>
+                                            </ul>
+                                        </li>
+                                        <li>World History</li>
+                                        <li>Biology</li>
+                                        <li>English 10</li>
+                                    </ul>
+                                </div>
+                            </section>
+                            <section>
+                                <h4>Grade 11: Theme: Identity</h4>
+                                <h5>Team Leader: <a href="teachers.php?t=neustaedter">Laura Neustaedter</a></h5>
+                                <p class="justify">
+                                    This year's curriculum is centered on the theme of Student Identity. Students create their own Identity Package which includes a personal logo, business stationery and business cards. In addition, students create a resumé and an All About Me brochure/website. In United States history and American literature, students focus on the individual's identity in American society, investigating and analyzing the life of an individual social activist in American history. The culminating interdisciplinary project is a comprehensive brochure or web site, based around a social activism event, which students present to lower grade level classes. This satisfies their service-learning requirement for graduation.
+                                </p>
+                                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#11th">View Classes</button>
+                                <div id="11th" class="collapse">
+                                    <h5><b>Classes</b></h5>
+                                    <ul>
+                                        <li>
+                                            Digital Imaging
+                                            <ul>
+                                                <li>Print</li>
+                                                <li>Web Design</li>
+                                            </ul>
+                                        </li>
+                                        <li>US History</li>
+                                        <li>Chemistry</li>
+                                        <li>English
+                                            <ul>
+                                                <li>American Literature</li>
+                                                <li>Contemportary Composition</li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </section>
+                            <section>
+                                <h4>Grade 12: Theme: Activism</h4>
+                                <h5>Team Leader: <a href="teachers.php?t=degracia">Wesley DeGracia</a></h5>
+                                <p class="justify">
+                                    The overall theme of the year is Social, Economic and Political Awareness. Students focus on a specific area in which they hope to effect change. They produce a research paper and brochure that explains and examines their understanding of and potential solutions for their issue. Literature, history and art classes coordinate efforts to present and encourage topics for exploration as well as the process that students will use to produce the requisite assignments. Students focus on their design area of concentration and are encouraged to take Advanced Placement courses as well as advanced art classes offered by the Art Department.
+                                </p>
+                                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#12th">View Classes</button>
+                                <div id="12th" class="collapse">
+                                    <h5><b>Classes</b></h5>
+                                    <ul>
+                                        <li>Graphic Design
+                                            <ul>
+                                                <li>Advance Print</li>
+                                                <li>Web Design</li>
+                                            </ul>
+                                        </li>
+                                        <li>
+                                            English
+                                            <ul>
+                                                <li>World Literature</li>
+                                                <li>Expository Composition</li>
+                                            </ul>
+                                        </li>
+                                        <li>Government</li>
+                                        <li>Economics</li>
+                                    </ul>
+                                </div>
+                            </section>
+                        </section>
+
+
+                    </section>
+                </article>
             </div>
         </section>
+
+
+
+
+
         <!--
         <section id="first_section">
             <div class="container-fluid lockcontent">
@@ -105,6 +265,7 @@
         </section>
 
         </section>-->
+        <!--
         <div class="container-fluid lockcontent gray_txt">
             <h3>Internships</h3>
         </div>
@@ -142,6 +303,8 @@
                 </p>
             </section>
         </section>
+-->
+        <!--
 
         <section class="blue_background">
             <section id="art_club" class="flex_b container-fluid lockcontent">
@@ -156,6 +319,8 @@
                 <img class="sec_img img" src="images/clubs/mentor.jpg" style="margin-left: 14px;margin-bottom: 9px;">
             </section>
         </section>
+-->
+        <!--
         <div class="container-fluid lockcontent gray_txt">
             <h3>AOAT Curriculum</h3>
         </div>
@@ -263,8 +428,8 @@
             </div>
 
         </section>
+-->
 
-        <!--end lockcontent-->
     </main>
     <footer>
         <?php        require 'php_dependancy/footer.php';
