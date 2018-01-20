023900650e839ee37fb9b6d3be85606d41f7cd6a
Bishoy Abdelmalik <mrboushy@users.noreply.github.com>
2018-01-19 22:52:16 -0800

    async index

--- v0.3/index.php
+++ v0.3/index.php
@@ -6,11 +6,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" href="css/styles.css" />
+    <link async rel="stylesheet" href="css/bootstrap.min.css" />
+    <link async rel="stylesheet" href="css/styles.css" />
     <title>AOAT | Home</title>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link async rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
